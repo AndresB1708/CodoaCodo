4d44913967141a4a6308086be55f97ad7b024d34
Andres <andresburgues@gmail.com>
2021-10-31 11:22:07 -0300

    Proyeco

--- js/valido.js
+++ js/valido.js
@@ -1,21 +1,80 @@
-function valida_envia(){
-    //valido el nombre
-    if (document.fvalida.validarNombre.value.length==0){
-           alert("Tiene que escribir su nombre")
-           document.fvalida.nombre.focus()
-           return 0;
-    }
-
-    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email) ? true : false;
-
+window.addEventListener('load', ()=> {
+    const form = document.querySelector('#formulario')
+    const validarNombre = document.getElementById('validarNombre')
+    const validarApellido = document.getElementById('validarApellido')
+    const email = document.getElementById('validarEmail')
+    const verificarEmail = document.getElementById('verificarEmail')
+    const validarMensaje = document.getElementById('validarMensaje')
     
-}
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        validaCampos()
+    })
+    
+    const validaCampos = ()=> {
 
-/*function validarEmail(validarEmail) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(validarEmail)){
-     alert("La dirección de email " + valor + " es correcta.");
-    } else {
-     alert("La dirección de email es incorrecta.");
+        const nombreValor = validarNombre.value.trim()
+        const apellidoValor = validarApellido.value.trim()
+        const emailValor = email.value.trim()
+        const emailConfirmaValor = verificarEmail.value.trim()
+        const mensajeValor = validarMensaje.value.trim()
+  
+       
+        if(!mensajeValor){
+            //console.log('CAMPO VACIO')
+            validaFalla(validarMensaje, 'Campo vacío')
+        }else{
+            validaOk(validarMensaje)
+        }
+        if(!nombreValor){
+            //console.log('CAMPO VACIO')
+            validaFalla(validarNombre, 'Campo vacío')
+        }else{
+            validaOk(validarNombre)
+        }
+        if(!apellidoValor){
+            //console.log('CAMPO VACIO')
+            validaFalla(validarApellido, 'Campo vacío')
+        }else{
+            validaOk(validarApellido)
+        }
+
+        //validando campo email
+        if(!emailValor){
+            validaFalla(email, 'Campo vacío')            
+        }else if(!validaEmail(emailValor)) {
+            validaFalla(email, 'El e-mail no es válido')
+        }else {
+            validaOk(email)
+        }
+
+        if(!emailConfirmaValor){
+            validaFalla(verificarEmail, 'Confirme su email') 
+        }else if(emailValor !== emailConfirmaValor){
+            validaFalla(verificarEmail, 'Los email no coinciden')
+        } else {
+            validaOk(verificarEmail)
+        }
+
+
+        
+        
     }
-  }*/
+    
+    const validaFalla = (input, msje) => {
+        const formControl = input.parentElement
+        const aviso = formControl.querySelector('p')
+        aviso.innerText = msje
+        
+        formControl.className = 'form-control falla'
+    }
+
+    const validaOk = (input, msje) => {
+        const formControl = input.parentElement
+        formControl.className = 'form-control ok'
+    }
+    const validaEmail = (email) => {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);        
+    }
+
+})
